044dc5a596b058132d4696e4b2e4dc3ff3650051
lorenseanstewart <l.s.stewart111@gmail.com>
2016-09-11 09:07:05 -0700

    added comments to route

--- server/router/routes/routes.js
+++ server/router/routes/routes.js
@@ -15,13 +15,40 @@ module.exports = (app, db) => {
       ]
     }).then(users => {
       const resObj = users.map(user => {
+        
+        //tidy up the user data
         return Object.assign(
           {},
           {
             user_id: user.id,
             username: user.username,
             role: user.role,
-            posts: user.posts
+            posts: user.posts.map(post => {
+
+              //tidy up the post data
+              return Object.assign(
+                {},
+                {
+                  post_id: post.id,
+                  user_id: post.user_id,
+                  content: post.content,
+                  comments: post.comments.map(comment => {
+
+                    //tidy up the comment data
+                    return Object.assign(
+                      {},
+                      {
+                        comment_id: comment.id,
+                        post_id: comment.post_id,
+                        commenter: comment.commenter_username,
+                        commenter_email: comment.commenter_email,
+                        content: comment.content
+                      }
+                    )
+                  })
+                }
+                )
+            })
           }
         )
       });
